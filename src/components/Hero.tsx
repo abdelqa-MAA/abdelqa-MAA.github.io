fe3371b4ed77bfe0516ded3fdc6f862b17d18786
abdelqa-MAA <abdelqa@pdx.edu>
2026-04-15 23:00:16 -0700

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <header className="px-8 pt-24 pb-32 md:pt-32 md:pb-48">
       <h1 className="max-w-[20ch] text-5xl md:text-7xl lg:text-8xl tracking-tighter leading-[1.1] font-medium text-balance font-heading">
-        Engineering the future, one circuit at a time.
+        Muhammad-Abdullah G. Abdelqader
       </h1>
       <p className="mt-10 md:mt-12 max-w-[50ch] text-lg text-clay leading-relaxed text-pretty">
         Electrical Engineering student at Portland State University passionate about embedded systems, 
